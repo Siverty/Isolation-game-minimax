da764b46fd6d1402a48cfbe6f1267023142adae4
NietMax <maxgoldsmits@gmail.com>
2023-09-30 21:53:01 +0200

    customized a bunch of stuff to make it ours

--- game.py
+++ game.py
@@ -8,14 +8,14 @@ from agent import Agent
 pygame.init()
 
 # Set the popup window size
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 800  # Increased window width
+HEIGHT = 800  # Increased window height
 
 # Set the size of the squares in the chess board
 SQUARE_SIZE = WIDTH // 6
 
 # Set the title of the popup window
-TITLE = "Isolation Game"
+TITLE = "Isolation Game Max And Justin"
 
 # Load the images of the two queens
 white_queen_img = pygame.image.load("images/white_queen.png")
@@ -32,7 +32,7 @@ clock = pygame.time.Clock()
 
 # Creating the queens
 white_queen = Queen(0, 0)
-black_queen = Queen(5, 5)
+black_queen = Queen(5, 5)  # Set the initial position of the black queen to (5, 5)
 
 # Defining that white starts
 white_turn = True
@@ -41,7 +41,7 @@ black_turn = False
 # Creating the board
 board = Board()
 board.block_pos(0, 0)
-board.block_pos(5, 5)
+board.block_pos(5, 5)  # Blocked position for the black queen
 
 # Starting a game
 game_state = True
@@ -52,8 +52,21 @@ running = True
 # This is to delay the time when the game ends
 switch = False
 
-# This is so the player is one game white and teh other game black
+# This is so the player is one game white and the other game black
 count_games = 0
+
+# Set the size and position of the board within the window
+BOARD_SIZE = min(WIDTH, HEIGHT) - 100  # Set the size of the board to fit within the window
+SQUARE_SIZE = BOARD_SIZE // 6  # Adjust the square size accordingly
+BOARD_X = (WIDTH - BOARD_SIZE) // 2  # Center the board horizontally
+BOARD_Y = (HEIGHT - BOARD_SIZE) // 2  # Center the board vertically
+
+# Set the board outline color
+BOARD_OUTLINE_COLOR = (0, 0, 0)  # Black color for the outline
+
+def draw_board_outline():
+    # Draw a rectangle to outline the board
+    pygame.draw.rect(window, BOARD_OUTLINE_COLOR, (BOARD_X - 2, BOARD_Y - 2, BOARD_SIZE + 4, BOARD_SIZE + 4), 4)
 
 def player_move():
     # Get the pos input from the user
@@ -62,32 +75,12 @@ def player_move():
 
     if click[0]:  # Left mouse button
         # Get position on x-axis
-        if mouse[0] <= 100:
-            mouse_pos_x = 0
-        elif mouse[0] <= 200:
-            mouse_pos_x = 1
-        elif mouse[0] <= 300:
-            mouse_pos_x = 2
-        elif mouse[0] <= 400:
-            mouse_pos_x = 3
-        elif mouse[0] <= 500:
-            mouse_pos_x = 4
-        elif mouse[0] <= 600:
-            mouse_pos_x = 5
+        if BOARD_X <= mouse[0] <= BOARD_X + BOARD_SIZE:
+            mouse_pos_x = (mouse[0] - BOARD_X) // SQUARE_SIZE
 
         # Get position on y-axis
-        if mouse[1] <= 100:
-            mouse_pos_y = 0
-        elif mouse[1] <= 200:
-            mouse_pos_y = 1
-        elif mouse[1] <= 300:
-            mouse_pos_y = 2
-        elif mouse[1] <= 400:
-            mouse_pos_y = 3
-        elif mouse[1] <= 500:
-            mouse_pos_y = 4
-        elif mouse[1] <= 600:
-            mouse_pos_y = 5
+        if BOARD_Y <= mouse[1] <= BOARD_Y + BOARD_SIZE:
+            mouse_pos_y = (mouse[1] - BOARD_Y) // SQUARE_SIZE
 
         # Change queen position
         if white_turn:
@@ -104,7 +97,7 @@ def player_move():
                 pass
 
 def agent_move():
-    n = 3  # THe agent will think n steps ahead
+    n = 3  # The agent will think n steps ahead
 
     # Change queen according to the best position
     if black_turn:
@@ -118,7 +111,6 @@ def agent_move():
         if white_queen.move(board, best_move[0], best_move[1]):
             return True
 
-
 while running:
     # Handle events
     for event in pygame.event.get():
@@ -130,7 +122,10 @@ while running:
         # Clear the window
         window.fill((255, 255, 255))
 
-        # Draw the chess board
+        # Draw the board outline
+        draw_board_outline()
+
+        # Draw the chess board within the specified size and position
         for x in range(6):
             for y in range(6):
                 # Determine if the square is available
@@ -138,7 +133,7 @@ while running:
                     if square_info["pos"] == (x, y):
                         if square_info["available"] == False:
                             if square_info["pos"] == (white_queen.pos_x, white_queen.pos_y) or \
-                                square_info["pos"] == (black_queen.pos_x, black_queen.pos_y):
+                                    square_info["pos"] == (black_queen.pos_x, black_queen.pos_y):
                                 # Make available squares black/white
                                 if (x + y) % 2 == 0:
                                     color = (150, 150, 150)  # Black
@@ -154,23 +149,31 @@ while running:
                             else:
                                 color = (255, 255, 255)  # White
 
-                # Draw the square
-                pygame.draw.rect(window, color, (x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                # Draw the square within the board's boundaries
+                pygame.draw.rect(window, color, (BOARD_X + x * SQUARE_SIZE, BOARD_Y + y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-        # Draw the white queen
-        window.blit(white_queen_img, (white_queen.pos_x * SQUARE_SIZE, white_queen.pos_y * SQUARE_SIZE))
+        # Draw the white queen within the board
+        window.blit(white_queen_img, (BOARD_X + white_queen.pos_x * SQUARE_SIZE, BOARD_Y + white_queen.pos_y * SQUARE_SIZE))
 
-        # Draw the black queen
-        window.blit(black_queen_img, (black_queen.pos_x * SQUARE_SIZE, black_queen.pos_y * SQUARE_SIZE))
+        # Draw the black queen within the board
+        window.blit(black_queen_img, (BOARD_X + black_queen.pos_x * SQUARE_SIZE, BOARD_Y + black_queen.pos_y * SQUARE_SIZE))
 
-        # # Define who's the player is playing
-        # if (count_games%2) == 0:
-        #     text = pygame.font.SysFont('arial', 40).render('You are white', True, (0, 0, 0))
-        # else:
-        #     text = pygame.font.SysFont('arial', 40).render('You are black', True, (0, 0, 0))
-        # pygame.draw.rect(window, (0, 150, 150), (0, 400, WIDTH, HEIGHT))
-        # window.blit(text, (
-        #     WIDTH / 2 - text.get_width() / 2, HEIGHT / 1.3 + text.get_height()))
+        # Define who's the player is playing
+        if (count_games % 2) == 0:
+            text = pygame.font.SysFont('arial', 40).render('You are white', True, (255, 255, 255))
+        else:
+            text = pygame.font.SysFont('arial', 40).render('You are black', True, (0, 0, 0))
+
+        # Calculate the Y-coordinate for the player info text
+        text_y = BOARD_Y - 50  # Adjust this value for vertical positioning
+
+        # Calculate the Y-coordinate for the player info rectangle
+        rect_y = BOARD_Y - 60+5  # added 5 pixels to make sure the text fits within the rectangle
+
+        # Draw the player info text and rectangle above the board with dark blue background
+        pygame.draw.rect(window, (50, 150, 50), (0, rect_y, WIDTH, text_y + text.get_height()))
+        window.blit(text, (
+            WIDTH / 2 - text.get_width() / 2, text_y))
 
         # Update the display
         pygame.display.update()
@@ -198,7 +201,7 @@ while running:
 
         # Move the queen
         elif white_turn:
-            if (count_games%2) == 0:
+            if (count_games % 2) == 0:
                 if player_move():
                     black_turn = True
                     white_turn = False
@@ -208,7 +211,7 @@ while running:
                     white_turn = False
 
         elif black_turn:
-            if (count_games%2) == 0:
+            if (count_games % 2) == 0:
                 if agent_move():
                     black_turn = False
                     white_turn = True
@@ -230,8 +233,8 @@ while running:
         window.fill((0, 0, 0))
         font = pygame.font.SysFont('arial', 40)
         title = font.render(queen_who_won + ' won the game', True, (255, 255, 255))
-        restart_button = font.render('R - Restart', True, (255, 255, 255))
-        quit_button = font.render('Q - Quit', True, (255, 255, 255))
+        restart_button = font.render('P - Play again', True, (255, 255, 255))
+        quit_button = font.render('S - Stop playing', True, (255, 255, 255))
         window.blit(title, (WIDTH / 2 - title.get_width() / 2, HEIGHT / 2 - title.get_height() / 2))
         window.blit(restart_button, (
             WIDTH / 2 - restart_button.get_width() / 2, HEIGHT / 1.9 + restart_button.get_height()))
@@ -243,12 +246,12 @@ while running:
         keys = pygame.key.get_pressed()
 
         # Restart game
-        if keys[pygame.K_r]:
+        if keys[pygame.K_p]:
             game_state = True
 
             # Creating the queens
             white_queen = Queen(0, 0)
-            black_queen = Queen(3, 3)
+            black_queen = Queen(5, 5)  # Set the initial position of the black queen to (5, 5)
 
             # Defining that white starts
             white_turn = True
@@ -257,10 +260,10 @@ while running:
             # Creating the board
             board = Board()
             board.block_pos(0, 0)
-            board.block_pos(3, 3)
+            board.block_pos(5, 5)  # Blocked position for the black queen
 
         # Quit game
-        if keys[pygame.K_q]:
+        if keys[pygame.K_s]:
             pygame.quit()
             quit()
 
